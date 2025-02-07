baa165c9a661c0eaa59d1ea23454b28aa6238ddb
Aatif <aatifkhan2001@gmail.com>
2025-02-07 08:58:23 +0530

    Added Second Smallest

--- secondLargest.py
+++ secondLargest.py
@@ -14,10 +14,22 @@ def secondLargest(arr,n):
             
     return sL
 
+def secondSmall(arr,n):
+    if n<2:
+        return -1
+    min = arr[0]
+    secondS = float('inf')  # Initialize to a very large value
+    for i in range(n):
+        if arr[i] < min:
+            secondS = min
+            min = arr[i]
+        elif arr[i]<secondS and arr[i]!=min:
+            secondS=arr[i]
+    return secondS
 
 
 
 
 arr = [1,2,3,4,5]
 n = len(arr)
-print(secondLargest(arr,n))
+print(secondSmall(arr,n))
