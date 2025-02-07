58e0a31aea17085f88440ebc4da3fd0484d3fa70
Aatif <aatifkhan2001@gmail.com>
2025-02-07 11:11:30 +0530

    Added removeDuplicate optimized

--- removeDup.py
+++ removeDup.py
@@ -10,9 +10,18 @@ def removeDup(arr,n):
         j+=1
     return k
         
+        
+def removeDupOptimized(nums,n):
+    i = 0
+    for j in range(1,len(nums)):
+        if nums[i]!=nums[j]:
+                i+=1
+                nums[i] = nums[j]
+
+    return i+1
 
 arr = [1,1,1,2,2,2,3,3,3]
 n = len(arr)
-ans = removeDup(arr,n)
+ans = removeDupOptimized(arr,n)
 for i in range(ans):
     print(arr[i])
