63d60a5cb374164931a9641f2fb1bb5c6ac3f752
Aatif <aatifkhan2001@gmail.com>
2025-02-09 15:46:02 +0530

    Added rightRotate

--- leftRotate.py
+++ leftRotate.py
@@ -8,12 +8,20 @@ def leftRotate(arr,n):
         arr[i] = arr[i+1]
         
     arr[n-1] = temp
-    
+
+
+
+def rightRotate(arr,n):
+    temp = arr[n-1]
+    for i in range(n-2,-1,-1):
+        arr[i+1] = arr[i]
+        
+    arr[0] = temp    
     
     
 arr = [1,2,3,4,5]
 n = len(arr)
-leftRotate(arr,n)
+rightRotate(arr,n)
 
 for i in range(n):
     print(arr[i])
